ef9ca4ab494ce01550fa125d65e9fab5ba0f289e
bhargavg13 <60029409+bhargavg13@users.noreply.github.com>
2020-01-18 00:44:13 -0800

    Create factorialCalculator.py
    This is a function file that calculates the factorial of an argument number.

---
+++ factorialCalculator.py
@@ -0,0 +1,18 @@
+def factorialnum(a):
+    if a < 0:
+        return print("Error input. Please input a positive integer only.")
+    elif isinstance(a, float):
+        return print("Error input. Please input a positive integer only.")
+    elif a == 0:
+        return print(1)
+    elif a == 1:
+        return print(1)
+    else:
+        j = a-1
+        facto = a
+        for x in range(1, j+1):
+            facto *= (a-x)
+        return print(facto)
+
+
+factorialnum(50)
